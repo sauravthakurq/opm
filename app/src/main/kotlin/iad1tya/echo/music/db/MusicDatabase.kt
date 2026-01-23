4f8b24a4a4bd04195b777759ad18459b23e82c97
sauravthakurq <sauravthakurq@gmail.com>
2026-01-23 20:42:54 +0530

    Update MusicDatabase.kt

--- app/src/main/kotlin/iad1tya/echo/music/db/MusicDatabase.kt
+++ app/src/main/kotlin/iad1tya/echo/music/db/MusicDatabase.kt
@@ -110,7 +110,7 @@ abstract class InternalDatabase : RoomDatabase() {
                 Room
                     .databaseBuilder(context, InternalDatabase::class.java, DB_NAME)
                     .addMigrations(MIGRATION_1_2)
-                    .fallbackToDestructiveMigration()
+                    .fallbackToDestructiveMigrationOnDowngrade(dropAllTables = true)
                     .build(),
             )
     }
