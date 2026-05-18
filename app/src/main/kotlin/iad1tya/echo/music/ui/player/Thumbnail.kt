9f3626c877ce18b57a7536121f8700f3ab2f9e3d
Saurav Thakur <sauravthakurq@gmail.com>
2026-05-18 19:14:27 +0530

    Add Image import to Thumbnail.kt
    Add missing import for androidx.compose.foundation.Image in Thumbnail.kt to resolve references and enable use of Image in the thumbnail composable.

--- app/src/main/kotlin/iad1tya/echo/music/ui/player/Thumbnail.kt
+++ app/src/main/kotlin/iad1tya/echo/music/ui/player/Thumbnail.kt
@@ -10,6 +10,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
